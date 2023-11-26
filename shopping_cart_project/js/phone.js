ef27bd6593a4bf3ccad5796e06cea3ab4deecd16
Hamim Ahmed <hamim.ahmed484@gmail.com>
2023-11-27 02:01:17 +0600

    shoping cart practice

---
+++ shopping_cart_project/js/phone.js
@@ -0,0 +1,31 @@
+function updatePhoneNumber(isIncrease){
+    const phoneNumberField = document.getElementById('phone-number-field');
+    const phoneNumberstring = phoneNumberField.value;
+    const previousPhoneNumber = parseInt(phoneNumberstring);
+    let newPhoneNumber;
+    if(isIncrease){
+        newPhoneNumber =  previousPhoneNumber+1;
+    }
+    else{
+        newPhoneNumber =  previousPhoneNumber-1;
+    }
+    phoneNumberField.value = newPhoneNumber;
+    return newPhoneNumber;
+
+}
+
+function updatePhonePrice(newphoneNumber){
+    const phoneTotalPrice = newphoneNumber * 1219;
+    const phoneTotalElement = document.getElementById('phone-total');
+    phoneTotalElement.innerText = phoneTotalPrice;
+} 
+
+document.getElementById('btn-phone-plus').addEventListener('click',function(){
+    const newphoneNumber = updatePhoneNumber(true);
+    updatePhonePrice(newphoneNumber);
+})
+
+document.getElementById('btn-phone-minus').addEventListener('click',function(){
+    const newphoneNumber = updatePhoneNumber(false);
+    updatePhonePrice(newphoneNumber);
+})
